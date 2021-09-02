7595987ca0908b298758c17d7d07437a1408d924
Thinh-Nguyen <thinh-nguyen@Thinh-Nguyens-MacBook-Pro.local>
2021-09-02 10:32:36 +0700

    Hoan thanh bai hoc thu 3 cua Gatsby

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import Layout from "./components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 
 const IndexPage = () => {
   return(
  <Layout pageTitle="Trang chủ">
    <p>Đây là trang chủ</p>
+   <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
  </Layout>
   )
 }
