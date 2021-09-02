df706c9b631f2fca983f91e645241c6da63bc35f
Thinh-Nguyen <thinh-nguyen@Thinh-Nguyens-MacBook-Pro.local>
2021-09-02 10:20:24 +0700

    Hoan thanh bai hoc Gatsby co ban 2

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './components/layout';
+
+const AboutPage = () => {
+    return(
+<Layout pageTitle="Giới thiệu">
+        <p>Đây là trang giới thiệu</p>
+</Layout>
+    )
+}
+
+export default AboutPage;
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "./components/layout";
+
+
+const IndexPage = () => {
+  return(
+ <Layout pageTitle="Trang chủ">
+   <p>Đây là trang chủ</p>
+ </Layout>
+  )
+}
+
+export default IndexPage
